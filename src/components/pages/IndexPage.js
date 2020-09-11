e8808961e5e018cc535a1505f2ed529c3eaddabc
Jiselle Liu <jiselle03@gmail.com>
2020-09-10 19:42:07 -0700

    Add search

--- src/components/pages/IndexPage.js
+++ src/components/pages/IndexPage.js
@@ -5,15 +5,16 @@ import axios from 'axios';
 import { baseUrl } from '../../config';
 import Flag from '../Flag';
 
-const IndexPage = props => {
+const IndexPage = () => {
     const [countries, setCountries] = useState([]);
+    const regions = ["Africa", "Americas", "Asia", "Europe", "Oceania"]
 
-    const getAll = () => {
-        return axios.get(`${baseUrl}/all`);
-    };
+    const getAll = () => axios.get(`${baseUrl}/all`);
+    const getRegion = region => axios.get(`${baseUrl}/region/${region}`);
+    const search = keyword => axios.get(`${baseUrl}/name/${keyword}`);
 
-    const search = keyword => {
-        return axios.get(`${baseUrl}/name/${keyword}`);
+    const filterByRegion = region => {
+        getRegion(region).then(countries => setCountries(countries.data));
     };
 
     useEffect(() => {
@@ -21,25 +22,44 @@ const IndexPage = props => {
     }, [countries]);
 
     return(
-        <main className="countries home">
-            {countries.map(country => (
-                <Link 
-                    key={country.name} 
-                    to={`/${country.alpha3Code}`} 
-                >
-                    <div className="country card">
-                        <Flag url={country.flag} />
+        <>
+            <div className="subheader">
+                <div className="search">
+                    Search
+                </div>
 
-                        <div className="summary">
-                            <h6>{country.name}</h6>
-                            <p><strong>Population:</strong> {country.population}</p>
-                            <p><strong>Region:</strong> {country.region}</p>
-                            <p><strong>Capital:</strong> {country.capital}</p>
-                        </div>
+                <div className="filter">
+                    <button className="dropbtn">Filter By Region</button>
+                    <div className="dropdown-content">
+                        {regions.map(region => (
+                            <p 
+                                key={region}
+                                // onClick={filterByRegion(region.toLowerCase())}
+                            >{region}</p>
+                        ))}
                     </div>
-                </Link>
-            ))}
-        </main>
+                </div>
+            </div>
+            <main className="countries home">
+                {countries.map(country => (
+                    <Link 
+                        key={country.name} 
+                        to={`/${country.alpha3Code}`} 
+                    >
+                        <div className="country card">
+                            <Flag url={country.flag} />
+
+                            <div className="summary">
+                                <h6>{country.name}</h6>
+                                <p><strong>Population:</strong> {country.population}</p>
+                                <p><strong>Region:</strong> {country.region}</p>
+                                <p><strong>Capital:</strong> {country.capital}</p>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </main>
+        </>
     );
 };
 
